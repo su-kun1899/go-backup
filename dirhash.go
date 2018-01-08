ec0f0fa5f454543d12dd0ba065096b67de2a2213
su-kun1899 <higedrum.coz@gmail.com>
2018-01-08 15:29:45 +0900

    8.2.3 ファイルシステムへの変更を検出する

---
+++ dirhash.go
@@ -0,0 +1,29 @@
+package backup
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"crypto/md5"
+)
+
+func DirHash(path string) (string, error) {
+	hash := md5.New()
+	err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		io.WriteString(hash, path)
+		fmt.Fprintf(hash, "%v", info.IsDir())
+		fmt.Fprintf(hash, "%v", info.ModTime())
+		fmt.Fprintf(hash, "%v", info.Mode())
+		fmt.Fprintf(hash, "%v", info.Name())
+		fmt.Fprintf(hash, "%v", info.Size())
+		return nil
+	})
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%x", hash.Sum(nil)), nil
+}
