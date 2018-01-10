5f1ab33290df3163a885aeda012e01bcfbb40d68
su-kun1899 <higedrum.coz@gmail.com>
2018-01-10 22:02:03 +0900

    8.3.1 少量のデータの永続化

--- cmds/backup/main.go
+++ cmds/backup/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"github.com/matryer/filedb"
 	"errors"
 	"log"
 	"flag"
 )
+
+type path struct {
+	Path string
+	Hash string
+}
 
 func main() {
 	var fatalErr error
@@ -21,6 +27,18 @@ func main() {
 	args := flag.Args()
 	if len(args) < 1 {
 		fatalErr = errors.New("エラー；コマンドを指定して下さい")
+		return
+	}
+
+	db, err := filedb.Dial(*dbpath)
+	if err != nil {
+		fatalErr = err
+		return
+	}
+	defer db.Close()
+	col, err := db.C("paths")
+	if err != nil {
+		fatalErr = err
 		return
 	}
 }
