78eabb786929457095040d601dc674d1b0415b33
su-kun1899 <higedrum.coz@gmail.com>
2018-01-13 23:32:07 +0900

    8.4.2 データのキャッシュ

--- cmds/backupd/main.go
+++ cmds/backupd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"encoding/json"
 	"github.com/su-kun1899/go-backup"
 	"flag"
 	"log"
@@ -40,6 +42,23 @@ func main() {
 	col, err := db.C("paths")
 	if err != nil {
 		fatalErr = err
+		return
+	}
+
+	var path path
+	col.ForEach(func(_ int, data []byte) bool {
+		if err := json.Unmarshal(data, &path); err != nil {
+			fatalErr = err
+			return true
+		}
+		m.Paths[path.Path] = [path.Hash]
+		return false // 処理を続行します
+	})
+	if fatalErr != nil {
+		return
+	}
+	if len(m.Paths) < 1 {
+		fatalErr = errors.New("パスがありません。backupツールを使って追加して下さい")
 		return
 	}
 }
