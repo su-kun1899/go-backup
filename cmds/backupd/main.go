12d27bdaa0bf0a21cf45cd61420a0bbaf8ce1cdf
su-kun1899 <higedrum.coz@gmail.com>
2018-01-14 12:22:19 +0900

    8.4.4 filedbのレコードの更新

--- cmds/backupd/main.go
+++ cmds/backupd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/matryer/filedb"
 	"fmt"
 	"time"
 	"syscall"
@@ -81,5 +82,33 @@ func main() {
 			log.Printf("終了します...")
 			break Loop
 		}
+	}
+}
+
+func check(m *backup.Monitor, col *filedb.C) {
+	log.Println("チェックします...")
+	counter, err := m.Now()
+	if err != nil {
+		log.Panicln("バックアップに失敗しました: ", err)
+	}
+	if counter > 0 {
+		log.Printf(" %d個のディレクトリをアーカイブしました\n", counter)
+		// ハッシュ値を更新します
+		var path path
+		col.SelectEach(func(_ int, data []byte) (bool, []byte, bool) {
+			if err := json.Unmarshal(data, &path); err != nil {
+				log.Println("JSONデータの読み込みに失敗しました。" + "次の項目に進みます: ", err)
+				return true, data, false
+			}
+			path.Hash, _ = m.Paths(path.Path)
+			newdata, err := json.Marshal(&path)
+			if err != nil {
+				log.Println("JSONデータの書き出しに失敗しました。" + "次の項目に進みます: ", err)
+				return true, data, false
+			}
+			return true, newdata, false
+		})
+	} else {
+		log.Println(" 変更はありません")
 	}
 }
